a08192264e5726726489fb54a042f617948272fb
Michael Collins <mfcollins3@me.com>
2022-11-28 09:17:52 -0700

    Integrate libssh2 1.10.0
    I added the source code for libssh2 1.10.0 as a Git submodule. I created
    the build.sh program to build libssh2 and produce the XCFramework that
    will be linked with macOS and iOS applications.
    
    I updated README.md with instructions on how to build libssh2.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # libssh2 Swift Packafge
 
+## Overview
+
 This repository implements a Swift package that builds, includes, and wraps the [libssh2](https://www.libssh2.org) library. I created this package because I have other frameworks that depend on libssh2 and I wanted to break out the libssh2 implementation in case anyone else had a need to use libssh2. Over time, I plan to build a Swift API for some of the libssh2 features to use them in applications.
 
 The libssh2 library that this package wraps was built on top of OpenSSL. For the OpenSSL package, please see my [openssl-apple](https://github.com/mfcollins3/openssl-apple) repository.
@@ -12,3 +14,20 @@ This Swift package supports the following platforms:
 * macOS Catalyst (Apple Silicone and Intel)
 
 :warning: Please note that in order to use this Swift package, you must also agree to the license terms for libssh2 and OpenSSL. Please review the [OpenSSL license](https://github.com/openssl/openssl/blob/openssl-3.0.7/LICENSE.txt) and the [libssh2 license](https://github.com/libssh2/libssh2/blob/libssh2-1.10.0/COPYING) for more information.
+
+## Building libssh2
+
+:warning: Building libssh2 from scratch requires you to have installed and built OpenSSL from my [openssl-apple](https://github.com/mfcollins3/openssl-apple) repository. `openssl-apple` and `libssh2-apple` must be siblings in the same parent directory as the build program in `libssh2-apple` uses a relative file path to find the `openssl-apple` repository.
+
+If you need to build the libssh2 libraries for use with other libraries that might depend on libssh2, I have provided the [build.sh](build.sh) script to automate the process. This program will build libssh2 for all supported platforms and will produce an XCFramework containing the libraries and header files for all supported platforms.
+
+This repository includes the source code for libssh2 as a Git submodule. To begin, you need to clone the repository and load the submodules:
+
+```sh
+git clone https://github.com/mfcollins3/libssh2-apple.git
+cd libssh2-apple
+git submodule init
+git submodule update
+```
+
+After cloning the repository and loading the libssh2 source code, you can run the `build.sh` program without any arguments to build the libssh2 library and produce the XCFramework.
---
+++ build.sh
@@ -0,0 +1,143 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 Michael F. Collins, III
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restrictions, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+# build.sh
+#
+# This program automated building libssh2 to be linked into an iOS or macOS
+# application, or to be used by other libraries that may be linked into
+# applications.
+#
+# Usage: build.sh
+
+ROOT_PATH=$PWD
+
+PLATFORMS="MAC MAC_ARM64 OS64 SIMULATOR64 SIMULATORARM64 MAC_CATALYST MAC_CATALYST_ARM64"
+for PLATFORM in $PLATFORMS; do
+    echo "Building libssh2 for $PLATFORM"
+
+    rm -rf /tmp/libssh2
+    cp -r External/libssh2 /tmp/
+
+    pushd /tmp/libssh2 > /dev/null
+
+    LOG=/tmp/libssh2-$PLATFORM.log
+    rm -rf $LOG
+
+    OUTPUT_PATH=$ROOT_PATH/build/$PLATFORM
+    rm -rf $OUTPUT_PATH
+
+    case $PLATFORM in
+        "MAC" )
+            OPENSSL_ROOT_DIR=$ROOT_PATH/../openssl-apple/build/darwin64-x86_64
+            ;;
+
+        "MAC_ARM64" )
+            OPENSSL_ROOT_DIR=$ROOT_PATH/../openssl-apple/build/darwin64-arm64
+            ;;
+
+        "OS64" )
+            OPENSSL_ROOT_DIR=$ROOT_PATH/../openssl-apple/build/openssl-ios64
+            ;;
+
+        "SIMULATOR64" )
+            OPENSSL_ROOT_DIR=$ROOT_PATH/../openssl-apple/build/openssl-iossimulator
+            ;;
+
+        "SIMULATORARM64" )
+            OPENSSL_ROOT_DIR=$ROOT_PATH/../openssl-apple/build/openssl-iossimulator-arm
+            ;;
+
+        "MAC_CATALYST" )
+            OPENSSL_ROOT_DIR=$ROOT_PATH/../openssl-apple/build/openssl-catalyst
+            ;;
+
+        "MAC_CATALYST_ARM64" )
+            OPENSSL_ROOT_DIR=$ROOT_PATH/../openssl-apple/build/openssl-catalyst-arm
+            ;;
+    esac
+
+    mkdir bin
+    cd bin
+    cmake \
+        -DCMAKE_TOOLCHAIN_FILE=$ROOT_PATH/External/ios-cmake/ios.toolchain.cmake \
+        -DPLATFORM=$PLATFORM \
+        -DCMAKE_INSTALL_PREFIX=$OUTPUT_PATH \
+        -DCRYPTO_BACKEND=OpenSSL \
+        -DOPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR \
+        -DENABLE_BITCODE=FALSE \
+        .. >> $LOG 2>&1
+    cmake --build . --target install >> $LOG 2>&1
+
+    popd > /dev/null
+done
+
+echo "Creating the universal library for macOS"
+
+OUTPUT_PATH=$ROOT_PATH/build/macos
+rm -rf $OUTPUT_PATH
+mkdir -p $OUTPUT_PATH
+lipo -create \
+    $ROOT_PATH/build/MAC/lib/libssh2.a \
+    $ROOT_PATH/build/MAC_ARM64/lib/libssh2.a \
+    -output $OUTPUT_PATH/libssh2.a
+
+echo "Creating the universal library for iOS Simulator"
+
+OUTPUT_PATH=$ROOT_PATH/build/iossimulator
+rm -rf $OUTPUT_PATH
+mkdir -p $OUTPUT_PATH
+lipo -create \
+    $ROOT_PATH/build/SIMULATOR64/lib/libssh2.a \
+    $ROOT_PATH/build/SIMULATORARM64/lib/libssh2.a \
+    -output $OUTPUT_PATH/libssh2.a
+
+echo "Creating the universal library for Catalyst"
+
+OUTPUT_PATH=$ROOT_PATH/build/catalyst
+rm -rf $OUTPUT_PATH
+mkdir -p $OUTPUT_PATH
+lipo -create \
+    $ROOT_PATH/build/MAC_CATALYST/lib/libssh2.a \
+    $ROOT_PATH/build/MAC_CATALYST_ARM64/lib/libssh2.a \
+    -output $OUTPUT_PATH/libssh2.a
+
+echo "Creating the libssh2 XCFramework"
+
+LIB_PATH=$ROOT_PATH
+LIBSSH2_PATH=$LIB_PATH/libssh2.xcframework
+rm -rf $LIBSSH2_PATH
+
+xcodebuild -create-xcframework \
+    -library $ROOT_PATH/build/macos/libssh2.a \
+    -headers $ROOT_PATH/build/MAC/include \
+    -library $ROOT_PATH/build/OS64/lib/libssh2.a \
+    -headers $ROOT_PATH/build/OS64/include \
+    -library $ROOT_PATH/build/iossimulator/libssh2.a \
+    -headers $ROOT_PATH/build/SIMULATOR64/include \
+    -library $ROOT_PATH/build/catalyst/libssh2.a \
+    -headers $ROOT_PATH/build/MAC_CATALYST/include \
+    -output $LIBSSH2_PATH
+
+zip -r libssh2.zip libssh2.xcframework
+
+echo "Done; cleaning up"
+rm -rf $TEMP_PATH
